6b9065c6889d162ec12d5538c33b7b926e713530
Durxoy115 <naimor35-2741@diu.edu.bd>
2022-03-27 13:31:00 +0600

    added cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,15 @@
 import React from "react";
+import "./Cart.css";
 
 const Cart = (cart) => {
   return (
-    <div>
+    <div className="cart">
       <h4>Order summary</h4>
       <p>Selected Item: {cart.cart.length}</p>
+      <p>Total price: </p>
+      <p>Shipping: </p>
+      <p>Tax: $</p>
+      <h5>Grand Total:</h5>
     </div>
   );
 };
